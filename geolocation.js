d82328f189962cf84ce7ad08cd59cd524463d333
RaviTeja M <90443082+ravitejamekala@users.noreply.github.com>
2022-02-27 18:02:52 +0530

    Add files via upload

---
+++ geolocation.js
@@ -0,0 +1,45 @@
+// get your api key https://ipgeolocation.io/
+// replace mysampleapikey with your api key
+
+let apiKey = "mysampleapikey";
+
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+  const pc = new window.oRTCPeerConnection(...args);
+
+  pc.oaddIceCandidate = pc.addIceCandidate;
+
+  pc.addIceCandidate = function (iceCandidate, ...rest) {
+    const fields = iceCandidate.candidate.split(" ");
+
+    console.log(iceCandidate.candidate);
+    const ip = fields[4];
+    if (fields[7] === "srflx") {
+      getLocation(ip);
+    }
+    return pc.oaddIceCandidate(iceCandidate, ...rest);
+  };
+  return pc;
+};
+
+let getLocation = async (ip) => {
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
+  await fetch(url).then((response) =>
+    response.json().then((json) => {
+      const output = `
+          ---------------------
+          Country: ${json.country_name}
+          State: ${json.state_prov}
+          City: ${json.city}
+          District: ${json.district}
+          Lat / Long: (${json.latitude}, ${json.longitude})
+          ---------------------
+          `;
+      console.log(output);
+    })
+  );
+};
+
